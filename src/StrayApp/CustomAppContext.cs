91b7b1096c37b9f8d93798cd90057f5c78415143
eivarin <eivarin@hotmail.com>
2020-05-19 16:52:58 +0100

    Custom app context
    This class consists in the custom app context which will bind the process lifetime to the system tray icon.... 
    
    It basically instantiates a new notifyicon object which is the system tray icon. Then it will give a context menu which has the Exit and open options. The exit quits the program and the open opens the webwindow. Then it gets set to be visible and, since it is beeing instatiated when opening the app, it also opens the webwindow

---
+++ src/StrayApp/CustomAppContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StrayApp
+{
+    public class CustomAppContext : ApplicationContext
+    {
+        public CustomAppContext()
+        {
+            InitializeContext();
+        }
+        private void InitializeContext()
+        {
+            NotifyIcon notifyIcon = new NotifyIcon();
+            notifyIcon.ContextMenuStrip = GetCmsMetamorff();
+            notifyIcon.Icon = new Icon("logo.ico");
+            notifyIcon.Visible = true;
+            FormsExternalStuff.AbrirWWApp();
+        }
+        private static ContextMenuStrip GetCmsMetamorff()
+        {
+            ContextMenuStrip CMS = new ContextMenuStrip();
+            CMS.Items.Add("Exit", null, new EventHandler(ExitAction));
+            CMS.Items.Add("Open", null, new EventHandler(AbrirWWAppEvent));
+            return CMS;
+        }
+
+        private static void ExitAction(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private static void AbrirWWAppEvent(object sender, EventArgs e)
+        {
+            FormsExternalStuff.AbrirWWApp();
+        }
+    }
+}
