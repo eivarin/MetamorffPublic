9f01da1ed365ce75b2bf2093946e6f719bd0632b
eivarin <eivarin@hotmail.com>
2020-05-19 17:23:39 +0100

    Project that exports a sample for testing......
    This is for me to have a sample which i can manipulate and quicly make a binary file to send to people to test the functionality i want

---
+++ src/TemporaryExport/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommonLib;
+using CommonLib.TorrentStuff;
+
+namespace TemporaryExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TorrentClientAPI.StartItAll();
+        }
+    }
+}
